3ab183e47c578c6d0dc8499d3c0c80251b038c95
Emilio Navarro <emilio.navarro@monsanto.com>
2019-04-05 11:08:11 -0500

    added helper urls

--- help.js
+++ help.js
@@ -5,3 +5,9 @@ console.log("Hey, there. Let's write some code.");
 console.log("First, install express by using: ");
 highlight('npm install express --save');
 highlight('npm install body-parser');
+
+//tsconfig.json url: https://raw.githubusercontent.com/emilionavarro/class-nodejs/phase6/tsconfig.json
+console.log("Get my tsconfig from here: https://raw.githubusercontent.com/emilionavarro/class-nodejs/phase6/tsconfig.json");
+
+//launch config: https://raw.githubusercontent.com/emilionavarro/class-nodejs/phase6/.vscode/launch.json
+console.log("Get my launch.json from here: https://raw.githubusercontent.com/emilionavarro/class-nodejs/phase6/.vscode/launch.json");
